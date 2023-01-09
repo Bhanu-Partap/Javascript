b02c803a3f5e19a56081dcebc1eaa3569e1b1445
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-01-09 14:02:13 +0530

    js

--- js/arrowfunction.js
+++ js/arrowfunction.js
@@ -1,9 +1,23 @@
+//============OLD METHOD OF FUNCTION====================//
+
+// console.clear();
+
+// const name = prompt();
+
+// function greet(name) {
+//   return `Hello ${name}`;
+// }
+
+// console.log(greet(name));
+
+//=============---------NEW METHOD USING ARROW FUNCTION===-----//
+
 console.clear();
 
 const name = prompt();
 
-function greet(name) {
+const greet = (name) => {
   return `Hello ${name}`;
-}
+};
 
 console.log(greet(name));
