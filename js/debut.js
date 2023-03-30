59525a1c22bf0d741f0fee7c9caf7ef3ae6149aa
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-03-30 16:01:50 +0530

    .

---
+++ js/debut.js
@@ -0,0 +1,17 @@
+document.write("Its nitro 5 here")
+const doc=true;
+console.log(typeof(doc));
+
+console.log("Its nitro 5 here")
+
+const obj = {
+    "Name" :"Bhanu",
+    "Age" : 21,
+    "Ph_No": 9855522998
+}
+
+console.log(obj.Name + " " + obj.Age + " " + obj.Ph_No);
+
+
+
+// console.log(typeof(obj.Age));
---
+++ js/settimeout.js
@@ -0,0 +1,11 @@
+let a = 5;
+setTimeout(() => {
+    console.log("Lamborghini")
+}, 5000);
+
+setTimeout(() => {
+    console.log("Verna")
+}, 2000);
+
+
+console.log(a)
