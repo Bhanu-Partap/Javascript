4aa6164fa4727206b0a663286f3e5d2da2d081ff
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-04-10 17:24:05 +0530

    practice

--- CRUD OPERATIONS/JS/CRUDOPR.js
+++ CRUD OPERATIONS/JS/CRUDOPR.js
@@ -1,20 +1,36 @@
 let addbtn = document.getElementById("button-addon2");
 addbtn.addEventListener("click", addchapter);
 
+let parentlist = document.getElementById("parent-list");
+
 function addchapter(e) {
+  if (parentlist.children[0].className == "emptyMsg") {
+    parentlist.children[0].remove();
+  }
   let currentBtn = e.currentTarget;
   let currentInput = currentBtn.previousElementSibling;
-    let currentChapter = currentInput.value;
+  let currentChapter = currentInput.value;
 
+  let newlist = document.createElement("li");
+  newlist.className = ("list-group-item d-flex justify-content-between");
 
-  let newlist = document.createElement('li');
-  newlist.classList.add("list-group-item");
-//   newlist.textContent = currentInput.value;
-  newlist.innerHTML =`<h3 class="flex-grow-1">${} </h3> 
+  //   newlist.textContent = currentInput.value;
+  newlist.innerHTML = `<h3 class="flex-grow-1">${currentChapter} </h3> 
   <button class="btn btn-warning mx-3">Edit</button>
-  <button class="btn btn-danger">Remove</button>`
-
-  let parentlist = document.getElementById("parent-list");
+  <button class="btn btn-danger" onclick="removechapter(this)">Remove</button>`;
+  
   parentlist.appendChild(newlist);
 }
-    
+
+// remove the list function
+
+function removechapter(currentElement) {
+  currentElement.parentElement.remove();
+  if (parentlist.children.length <= 0) {
+    let newEmptymsg = document.createElement("h3");
+    newEmptymsg.classList.add("emptyMsg");
+    newEmptymsg.textContent = " Nothing is here please add a chappter";
+    // newEmptymsg.className = ("d-flex justify-content-center");
+    parentlist.appendChild(newEmptymsg);
+  }
+}
